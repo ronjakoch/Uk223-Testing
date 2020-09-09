beb0b932ed034fad484f730d2c0fe126361a3af4
ronja <ronjakoch@gmx.ch>
2020-09-09 16:39:42 +0200

    2nd commit

--- src/main/java/ch/noseryoung/uk/UkApplication.java
+++ src/main/java/ch/noseryoung/uk/UkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UkApplication {
 
+	//startOfProgramm
 	public static void main(String[] args) {
 		SpringApplication.run(UkApplication.class, args);
 	}
